358acd567d26bfa06ddcf7c55f37e64b80cd3527
Brian Cain <brianccain@gmail.com>
2015-04-12 16:51:41 -0700

    Change cookieleaks vhost to proxy

--- manifests/cookieleaks.pp
+++ manifests/cookieleaks.pp
@@ -2,26 +2,10 @@
 # Class to setup cookieleaks.org
 #
 class briancainnet::cookieleaks {
-  $cookieleaks_root = '/var/www/cookieleaks'
 
   class { 'nginx': }
 
-  file { '/var/www' :
-    ensure  => directory,
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0755',
-    require => [ Package['nginx'], ],
-  }
-
   nginx::resource::vhost { ['www.cookieleaks.org', 'cookieleaks.org'] :
-    www_root => $cookieleaks_root,
-  }
-
-  file { $cookieleaks_root :
-    ensure => directory,
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0775',
+    proxy => 'http://localhost:5432',
   }
 }

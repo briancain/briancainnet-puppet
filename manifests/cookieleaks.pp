07df7689d2d74c44a22c602536312ae4f2b025c7
Brian Cain <brianccain@gmail.com>
2015-04-26 15:19:57 -0700

    Add cookieleaks deployment

--- manifests/cookieleaks.pp
+++ manifests/cookieleaks.pp
@@ -2,7 +2,26 @@
 # Class to setup cookieleaks.org
 #
 class briancainnet::cookieleaks {
+  $packages = [
+    'unzip',
+  ]
+
+  package { $packages : ensure => present, }
+
   $cookieleaks_root = '/var/www/cookieleaks'
+  $cookieleaks_repo_root = '/home/brian/projects/cookieleaks.org'
+
+  Vcsrepo {
+    owner    => 'brian',
+    provider => git,
+    ensure   => present,
+  }
+
+  vcsrepo { $cookieleaks_repo_root:
+    source   => 'https://www.github.com/briancain/cookieleaks.org',
+    ensure   => latest,
+    revision => master,
+  }
 
   class { 'nginx': }
 
@@ -23,5 +42,24 @@ class briancainnet::cookieleaks {
     owner  => 'www-data',
     group  => 'www-data',
     mode   => '0775',
+  }
+
+  # lol deployments
+  exec { 'extract cookieleaks artifact':
+    command => "/usr/bin/unzip -o ${cookieleaks_repo_root}/files/site.zip",
+    path    => "${cookieleaks_repo_root}/files",
+    user    => 'root',
+    cwd     => "${cookieleaks_repo_root}/files",
+    require => [ Vcsrepo[$cookieleaks_repo_root], Package['unzip'] ],
+  }
+
+  file { $cookieleaks_root :
+    ensure  => directory,
+    recurse => true,
+    source  => "${cookieleaks_repo_root}/files/_site",
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0755',
+    require => Exec['extract cookieleaks artifact'],
   }
 }
